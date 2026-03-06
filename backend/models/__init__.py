01310077a2ad87c701bcdc642ee1aec1b80484f5
vedanshi-28 <vedanshichovatiya@gmail.com>
2026-03-06 12:57:55 +0530

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Models Package
-Contains ML models and data structures
-"""
-
-from .emotion_model import EmotionModel
-
-__all__ = ['EmotionModel']
--- backend/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-﻿"""
-Tests Package
-Unit tests for NLP modules
-"""
-
-# This file marks the tests directory as a Python package
-# It can be empty or contain test configuration
-
-__all__ = []
